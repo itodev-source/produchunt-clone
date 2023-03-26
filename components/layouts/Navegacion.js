a0d0d7bdbfb7256e572ecc98616abd7e4bc0282d
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-03-26 19:24:51 -0300

    pasando navegacion

---
+++ components/layouts/Navegacion.js
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+import Link from "next/link";
+import React from "react";
+
+const Nav = styled.nav`
+  padding-left: 2rem;
+
+  a{
+    font-size: 1.8rem;
+    margin-left: 2rem;
+    color: var(--gris2);
+    font-family: "PT Sans", sans-serif;
+  }
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
+
+const Navegacion = () => {
+  return (
+    <Nav>
+      <Link href="/">Inicio</Link>
+      <Link href="/populares">Populares</Link>
+      <Link href="/nuevo-producto">Nuevos Productos</Link>
+    </Nav>
+  );
+};
+
+export default Navegacion;
