5f1e345c04393dcf616d592424a4b710f70f6641
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-03-28 13:19:30 -0300

    creando formulario

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,14 +1,40 @@
-import Layout from '@/components/layouts/Layout';
-import React from 'react'
+import Layout from "@/components/layouts/Layout";
+import React from "react";
 
 const crearCuenta = () => {
   return (
     <div>
-        <Layout>
-            <h1>Crear Cuenta</h1>
-        </Layout>
+      <Layout>
+        <h1>Crear Cuenta</h1>
+
+        <form>
+          <div>
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              placeholder="Tu Nombre"
+              name="nombre"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" placeholder="Tu Email" name="email" />
+          </div>
+          <div>
+            <label htmlFor="email">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Tu Password"
+              name="password"
+            />
+          </div>
+          <input type="submit" value="Crear Cuenta" />
+        </form>
+      </Layout>
     </div>
-  )
-}
+  );
+};
 
 export default crearCuenta;
