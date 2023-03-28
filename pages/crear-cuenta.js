89a916b3dce22b8c1a3d8fc35beb36cf7f7a0e68
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-03-28 13:48:32 -0300

    ocupando custom hook y validacion

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -3,14 +3,35 @@ import { css } from "@emotion/core";
 import Layout from "@/components/layouts/Layout";
 import { Formulario, Campo, InputSubmit } from "@/components/ui/Formulario";
 
+// validaciones
+import useValidacion from "../hooks/useValidacion";
+import validarCrearCuenta from "../validacion/validarCrearCuenta";
+
+const STATE_INICIAL = {
+  nombre: "",
+  email: "",
+  password: "",
+};
+
 const crearCuenta = () => {
+  const { valores, errores, submitForm, handleSubmit, handleChange } =
+    useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+
+  function crearCuenta() {
+    console.log("creando cuenta!");
+  }
+
   return (
     <div>
       <Layout>
-        <h1 css={css`
-          text-align: center;
-          margin-top: 5rem;
-        `}>Crear Cuenta</h1>
+        <h1
+          css={css`
+            text-align: center;
+            margin-top: 5rem;
+          `}
+        >
+          Crear Cuenta
+        </h1>
 
         <Formulario>
           <Campo>
@@ -24,12 +45,7 @@ const crearCuenta = () => {
           </Campo>
           <Campo>
             <label htmlFor="email">Email</label>
-            <input 
-              type="text" 
-              id="email" 
-              placeholder="Tu Email" 
-              name="email" 
-            />
+            <input type="text" id="email" placeholder="Tu Email" name="email" />
           </Campo>
           <Campo>
             <label htmlFor="email">Password</label>
