d01e9b8f46f438221c305380543c64c52b065e61
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-03-28 19:26:52 -0300

    insertar datos en firestore

--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { css } from "@emotion/core";
 import Layout from "@/components/layouts/Layout";
-import Router from "next/router";
+import Router, {useRouter} from "next/router";
 import {
   Formulario,
   Campo,
@@ -9,7 +9,7 @@ import {
   Error,
 } from "@/components/ui/Formulario";
 
-import firebase from "@/firebase";
+import { FirebaseContext } from "@/firebase";
 
 // validaciones
 import useValidacion from "../hooks/useValidacion";
@@ -33,11 +33,38 @@ const NuevoProducto = () => {
     handleSubmit,
     handleChange,
     handleBlur,
-  } = useValidacion(STATE_INICIAL, validarCrearProducto, crearCuenta);
+  } = useValidacion(STATE_INICIAL, validarCrearProducto, crearProducto);
 
   const { nombre, empresa, imagen, url, descripcion } = valores;
 
-  async function crearCuenta() {
+  // hook de routing para redireccionar 
+  const router = useRouter()
+
+  // context con las operaciones crud
+
+  const { usuario, firebase } = useContext(FirebaseContext);
+
+  async function crearProducto() {
+    // si el usuario no esta autenticado llevar al login
+    if(!usuario){
+      return router.push("/login")
+    }
+
+    // crear el objeto de nuevo producto
+
+    const producto = {
+      nombre,
+      empresa,
+      url,
+      descripcion,
+      votos: 0,
+      comentarios: [],
+      creado: Date.now()
+    }
+
+    // insertarlo en la base de datos
+    firebase.db.collection("productos").add(producto)
+  
   }
 
   return (
@@ -55,35 +82,35 @@ const NuevoProducto = () => {
         <Formulario onSubmit={handleSubmit} noValidate>
           <fieldset>
             <legend>Informacion General</legend>
-          <Campo>
-            <label htmlFor="nombre">Nombre</label>
-            <input
-              type="text"
-              id="nombre"
-              placeholder="Tu Nombre"
-              name="nombre"
-              value={nombre}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          </Campo>
-          {errores.nombre && <Error>{errores.nombre}</Error>}
+            <Campo>
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                id="nombre"
+                placeholder="Tu Nombre"
+                name="nombre"
+                value={nombre}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+            {errores.nombre && <Error>{errores.nombre}</Error>}
 
-          <Campo>
-            <label htmlFor="empresa">Empresa</label>
-            <input
-              type="text"
-              id="empresa"
-              placeholder="Tu Empresa"
-              name="empresa"
-              value={empresa}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          </Campo>
-          {errores.empresa && <Error>{errores.empresa}</Error>}
+            <Campo>
+              <label htmlFor="empresa">Empresa</label>
+              <input
+                type="text"
+                id="empresa"
+                placeholder="Tu Empresa"
+                name="empresa"
+                value={empresa}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+            {errores.empresa && <Error>{errores.empresa}</Error>}
 
-          {/* <Campo>
+            {/* <Campo>
             <label htmlFor="imagen">Imagen</label>
             <input
               type="file"
@@ -96,34 +123,34 @@ const NuevoProducto = () => {
           </Campo>
           {errores.imagen && <Error>{errores.imagen}</Error>} */}
 
-          <Campo>
-            <label htmlFor="url">URL</label>
-            <input
-              type="url"
-              id="url"
-              name="url"
-              placeholder="URL de tu producto"
-              value={url}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          </Campo>
-          {errores.url && <Error>{errores.url}</Error>}
+            <Campo>
+              <label htmlFor="url">URL</label>
+              <input
+                type="url"
+                id="url"
+                name="url"
+                placeholder="URL de tu producto"
+                value={url}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+            {errores.url && <Error>{errores.url}</Error>}
           </fieldset>
-         
-         <fieldset>
-          <legend>Sobre tu producto</legend>
-          <Campo>
-            <label htmlFor="descripcion">Descripcion</label>
-            <textarea
-              id="descripcion"
-              name="descripcion"
-              value={descripcion}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          </Campo>
-          {errores.descripcion && <Error>{errores.descripcion}</Error>}
+
+          <fieldset>
+            <legend>Sobre tu producto</legend>
+            <Campo>
+              <label htmlFor="descripcion">Descripcion</label>
+              <textarea
+                id="descripcion"
+                name="descripcion"
+                value={descripcion}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+            {errores.descripcion && <Error>{errores.descripcion}</Error>}
           </fieldset>
           {error && <Error>{error}</Error>}
           <InputSubmit type="submit" value="Crear Producto" />
