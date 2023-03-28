16f08a7374e3b3b842890f91dc175ec02ce869e1
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-03-28 14:20:26 -0300

    agregando blur (cuando no hay enfoque)

--- hooks/useValidacion.js
+++ hooks/useValidacion.js
@@ -1,45 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const useValidacion = (stateInicial, validar, fn) => {
-    const [valores, guardarValores] = useState(stateInicial);
-    const [errores, guardarErrores] = useState({})
-    const [submitForm, guardarSubmitForm] = useState(false)
+  const [valores, guardarValores] = useState(stateInicial);
+  const [errores, guardarErrores] = useState({});
+  const [submitForm, guardarSubmitForm] = useState(false);
 
-    useEffect(() => {
-        if(submitForm) {
-            const noErrores = Object.keys(errores).length === 0; // Recorre el objeto en busca si no hay un dato
+  useEffect(() => {
+    if (submitForm) {
+      const noErrores = Object.keys(errores).length === 0; // Recorre el objeto en busca si no hay un dato
 
-            if(noErrores){
-                fn(); // Funcion que se ejecuta en el componente
-            }
-            guardarSubmitForm(false)
-        }
-    }, [errores])
-    
-    // Funcion que se ejecuta conforme el usuario escribe algo
-    const handleChange = e=> {
-        guardarValores({
-            ...valores,
-            [e.target.name]: e.target.value
-        })
+      if (noErrores) {
+        fn(); // Funcion que se ejecuta en el componente
+      }
+      guardarSubmitForm(false);
     }
+  }, [errores]);
 
-    // Funcion que se ejecuta cuando el usuario hace submit
-    
-    const handleSubmit = e => {
-        e.preventDefault();
-        const erroresValidacion = validar(valores)
-        guardarErrores(erroresValidacion)
-        guardarSubmitForm(true)
-    }
+  // Funcion que se ejecuta conforme el usuario escribe algo
+  const handleChange = (e) => {
+    guardarValores({
+      ...valores,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Funcion que se ejecuta cuando el usuario hace submit
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const erroresValidacion = validar(valores);
+    guardarErrores(erroresValidacion);
+    guardarSubmitForm(true);
+  };
+
+  // cuando se realiza el evento blur (sacar el enfoque del input)
+  const handleBlur = () => {
+    const erroresValidacion = validar(valores);
+    guardarErrores(erroresValidacion);
+  };
 
   return {
     valores,
     errores,
     submitForm,
     handleSubmit,
-    handleChange    
-  }
-}
+    handleChange,
+    handleBlur
+  };
+};
 
-export default useValidacion
+export default useValidacion;
