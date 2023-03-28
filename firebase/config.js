03852d7fa8c138580402e0dc1bdfa25c801249b8
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-03-28 15:32:50 -0300

    implementando firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBdRVdTyna663UBkMjAaPJ-cosgzVaJU7E",
+  authDomain: "product-hunt-67356.firebaseapp.com",
+  projectId: "product-hunt-67356",
+  storageBucket: "product-hunt-67356.appspot.com",
+  messagingSenderId: "675272359369",
+  appId: "1:675272359369:web:68d0aee9e73a7cd6e4bc18",
+};
+
+export default firebaseConfig;
